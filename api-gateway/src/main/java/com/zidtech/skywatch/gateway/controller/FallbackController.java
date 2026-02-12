3611fe63c01c7e7cc322c587d8e36c4e0e353918
zidanali111 <zidanali111@gmail.com>
2026-02-13 00:15:44 +0530

    Fallback Controller code changes

---
+++ api-gateway/src/main/java/com/zidtech/skywatch/gateway/controller/FallbackController.java
@@ -0,0 +1,14 @@
+package com.zidtech.skywatch.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("Service temporarily unavailable. Please try again later.");
+    }
+}
