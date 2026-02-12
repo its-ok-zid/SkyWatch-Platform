29df1d7b63af9a7831f7a4ac1b64ed60b8e4f577
zidanali111 <zidanali111@gmail.com>
2026-02-12 23:48:57 +0530

    pom.xml configuration setup

---
+++ api-gateway/src/main/java/com/zidtech/skywatch/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zidtech.skywatch.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/com/zidtech/skywatch/SkyWatchPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zidtech.skywatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyWatchPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
